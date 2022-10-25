28c682965662b1b51ee6a237f6dfea2a89496f8f
letuan279 <latuan388@gmail.com>
2022-10-25 17:14:55 +0700

    week 1

---
+++ prj1/Prj1/src/CalculateTwoNumbers.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class CalculateTwoNumbers {
+    public static void main(String[] args) {
+        String strNum1, strNum2;
+        Double douNum1, douNum2;
+        String strNotification;
+
+        strNum1 = JOptionPane.showInputDialog(null, "Please input the first number: ", "Input the first number",
+                JOptionPane.INFORMATION_MESSAGE);
+        douNum1 = Double.parseDouble(strNum1);
+
+        strNum2 = JOptionPane.showInputDialog(null, "Please input the first number: ", "Input the first number",
+                JOptionPane.INFORMATION_MESSAGE);
+        douNum2 = Double.parseDouble(strNum2);
+
+        strNotification = "Sum: " + (douNum1 + douNum2) + "\n"
+                + "Difference: " + (douNum1 - douNum2) + "\n"
+                + "Product: " + (douNum1 * douNum2) + "\n"
+                + "Quotient: " + (douNum1 / douNum2);
+
+        JOptionPane.showMessageDialog(null, strNotification, "Calculation", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
+}
