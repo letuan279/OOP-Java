c1eeb40bf9b7022f21449a6b309b7abdb8af29e2
tuanla-0242 <le.anh.tuan-c@sun-asterisk.com>
2022-11-25 17:19:34 +0700

    add memory manage string

---
+++ OtherProjects/src/hust/soict/dsai/lab01/Star.java
@@ -0,0 +1,16 @@
+package hust.soict.dsai.lab01;
+
+public class Star {
+    public static void main(String[] args) {
+        int line = 5;
+        for (int i = 0; i < line; i++) {
+            for (int j = 0; j < 2 * line - 1; j++) {
+                if (j >= (2 * line - 1) / 2 - i && j <= (2 * line - 1) / 2 + i) {
+                    System.out.print("*");
+                } else
+                    System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
